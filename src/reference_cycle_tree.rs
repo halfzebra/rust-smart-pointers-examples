d0f57ba42e7466e89ac79f06a9ce5a0f97765967
Eduard Kyvenko <eduard.kyvenko@gmail.com>
2019-11-25 12:31:33 +0100

    Finalize the example of using the Weak ref

--- src/reference_cycle_tree.rs
+++ src/reference_cycle_tree.rs
@@ -19,6 +19,7 @@ impl Node {
 }
 
 fn print_ref_count(label: &str, node: &Rc<Node>) {
+    // pass a reference to a Rc reference to avoid affecting Rc strong_count and weak_count increase
     println!("{} strong: {} weak: {}", label, Rc::strong_count(&node), Rc::weak_count(&node));
 }
 
@@ -29,24 +30,26 @@ pub fn example() {
         // We can create an empty Weak reference, which will .upgrade() to None
         RefCell::new(Weak::new()),
     ));
-    let branch = Rc::new(Node::new(
-        5,
-        RefCell::new(vec![Rc::clone(&leaf)]),
-        RefCell::new(Weak::new()),
-    ));
-    println!("leaf {:?}", leaf);
-    print_ref_count("leaf", &leaf);
-    // We need to .borrow() the value insode the RefCell first
+    print_ref_count("leaf created", &leaf);
+
+    // create a scope to simulate branch being destroyed
+    {
+        let branch = Rc::new(Node::new(
+            5,
+            RefCell::new(vec![Rc::clone(&leaf)]),
+            RefCell::new(Weak::new()),
+        ));
+
+        // println!("leaf after branch created {:?}", leaf);
+
+        // We .downgrade() the branch to weak ref
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+        print_ref_count("branch", &branch);
+        print_ref_count("leaf", &leaf);
+    }
+
+    // We need to .borrow() the value inside the RefCell first
     println!("leaf.parent {:?}", leaf.parent.borrow().upgrade());
-    println!("branch {:?}", branch);
-
-    // We .downgrade() the branch to weak ref
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
-
-    print_ref_count("branch", &branch);
-    print_ref_count("leaf", &leaf);
-
-    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
-
     print_ref_count("leaf", &leaf);
 }
